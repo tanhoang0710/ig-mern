29c9ef52ecd5fe3411a2267f024e5562e1b0fdc2
tanhun <tanhoang0710@gmail.com>
2023-01-27 10:36:59 +0700

    init commit

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
